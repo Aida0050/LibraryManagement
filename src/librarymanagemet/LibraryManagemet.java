cf0ba9915d2d923a6af3aadcf4e5efc81dffca14
Aida0050 <Aida.zace1@outlook.com>
2024-10-24 16:47:46 +0200

    first commit

---
+++ src/librarymanagemet/LibraryManagemet.java
@@ -0,0 +1,19 @@
+
+package librarymanagemet;
+
+
+public class LibraryManagemet {
+
+   
+    public static void main(String[] args) {
+      
+        
+                Login LoginFrame=new Login();
+                LoginFrame.setVisible(true);
+                LoginFrame.pack();
+                LoginFrame.setLocationRelativeTo(null);
+            
+    
+    }
+    
+}
